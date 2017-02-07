e7c9cdf6b12aae85896b6f4874a4f13047353535
xukun <1158674366@qq.com>
2017-02-07 16:42:26 +0800

    add audio function

---
+++ Diary/Edit/Controller/audioImage.h
@@ -0,0 +1,15 @@
+//
+//  audioImage.h
+//  Diary
+//
+//  Created by xukun on 2017/2/6.
+//  Copyright © 2017年 xukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface audioImage : UIView
+
+
+-(void)builderViewWithSceond:(NSTimeInterval)second;
+@end
