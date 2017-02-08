4a513cfe37e354bdb61220a201c0deca963ba673
hanserHe <94189786@qq.com>
2017-02-08 14:35:10 +0800

    no message

--- Diary/SecondItem/Controller/SecondItemViewController.m
+++ Diary/SecondItem/Controller/SecondItemViewController.m
@@ -33,7 +33,7 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     [self initTableView];
-    [self initDataSource];
+   // [self initDataSource];
     
  
 }
@@ -66,7 +66,7 @@
 
 -(UITableView *)groupTable{
     if (!_groupTable) {
-        _groupTable = [[UITableView alloc] initWithFrame:CGRectMake(0, kNavigationBarHeight, kScreenWidth, kScreenHeight) style:UITableViewStylePlain];
+        _groupTable = [[UITableView alloc] initWithFrame:CGRectMake(0, kNavigationBarHeight, kScreenWidth, kScreenHeight)];
         _groupTable.delegate = self;
         _groupTable.dataSource = self;
         _groupTable.separatorStyle = UITableViewCellSeparatorStyleNone;
@@ -76,89 +76,91 @@
 
 #pragma mark --------------------------------------------------------------
 #pragma mark - UITableViewDelegate && UITableViewDataSource
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return _dataArray.count;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 1;
+//}
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    NSMutableDictionary *dic=[_dataArray objectAtIndex:section];
+//    NSMutableDictionary *dic=[_dataArray objectAtIndex:section];
     
-    NSArray *array=[dic objectForKey:DIC_ARARRY];
-    
-    //判断是收缩还是展开
-    
-    if ([[dic objectForKey:DIC_EXPANDED] intValue]) {
-        
-        return array.count;
-        
-    }else{
-        return 0;
-    }
-
+//    NSArray *array=[dic objectForKey:DIC_ARARRY];
+//    
+//    //判断是收缩还是展开
+//    
+//    if ([[dic objectForKey:DIC_EXPANDED] intValue]) {
+//        
+//        return array.count;
+//        
+//    }else{
+//        return 0;
+//    }
+    return 10;
 }
 
--(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 50;
 }
 
 
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    
-    UIView *hView = [[UIView alloc]initWithFrame:CGRectMake(0,0, self.view.bounds.size.width, CELL_HEIGHT)];
-    
-    hView.backgroundColor=[UIColor whiteColor];
-    
-    UIButton* eButton = [[UIButton alloc] init];
-    
-    //按钮填充整个视图
-    eButton.frame = hView.frame;
-    
-    [eButton addTarget:self action:@selector(expandButtonClicked:)
-     
-      forControlEvents:UIControlEventTouchUpInside];
-    
-    //把节号保存到按钮tag，以便传递到expandButtonClicked方法
-    
-    eButton.tag = section;
-    
-    //设置图标
-    
-    //根据是否展开，切换按钮显示图片
-    
-    if ([self isExpanded:section]){
-        
-        [eButton setImage: [UIImage imageNamed: @"arrow_right_grey" ]forState:UIControlStateNormal];
-    } else {
-        
-        [eButton setImage: [UIImage imageNamed: @"arrow_down_grey" ]forState:UIControlStateNormal];
-    }
-    //设置分组标题
-    
-    [eButton setTitle:[[_dataArray objectAtIndex:section] objectForKey:DIC_TITILESTRING] forState:UIControlStateNormal];
-    
-    [eButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    
-    //设置button的图片和标题的相对位置
-    
-    //4个参数是到上边界，左边界，下边界，右边界的距离
-    
-    eButton.contentHorizontalAlignment =UIControlContentHorizontalAlignmentLeft;
-    
-    [eButton setTitleEdgeInsets:UIEdgeInsetsMake(5,5, 0,0)];
-    
-    [eButton setImageEdgeInsets:UIEdgeInsetsMake(5,self.view.bounds.size.width - 25, 0,0)];
-    
-    //下显示线
-    
-    UILabel *label=[[UILabel alloc] initWithFrame:CGRectMake(0, hView.frame.size.height-1, hView.frame.size.width,1)];
-    
-    label.backgroundColor = [UIColor grayColor];
-    [hView addSubview:label];
-    
-    [hView addSubview: eButton];
-    
-    return hView;
-}
+//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+//    
+//    UIView *hView = [[UIView alloc]initWithFrame:CGRectMake(0,0, self.view.bounds.size.width, CELL_HEIGHT)];
+//    
+//    hView.backgroundColor=[UIColor whiteColor];
+//    
+//    UIButton* eButton = [[UIButton alloc] init];
+//    
+//    //按钮填充整个视图
+//    eButton.frame = hView.frame;
+//    
+//    [eButton addTarget:self action:@selector(expandButtonClicked:)
+//     
+//      forControlEvents:UIControlEventTouchUpInside];
+//    
+//    //把节号保存到按钮tag，以便传递到expandButtonClicked方法
+//    
+//    eButton.tag = section;
+//    
+//    //设置图标
+//    
+//    //根据是否展开，切换按钮显示图片
+//    
+//    if ([self isExpanded:section]){
+//        
+//        [eButton setImage: [UIImage imageNamed: @"arrow_right_grey" ]forState:UIControlStateNormal];
+//    } else {
+//        
+//        [eButton setImage: [UIImage imageNamed: @"arrow_down_grey" ]forState:UIControlStateNormal];
+//    }
+//    //设置分组标题
+//    
+//    [eButton setTitle:[[_dataArray objectAtIndex:section] objectForKey:DIC_TITILESTRING] forState:UIControlStateNormal];
+//    
+//    [eButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    
+//    //设置button的图片和标题的相对位置
+//    
+//    //4个参数是到上边界，左边界，下边界，右边界的距离
+//    
+//    eButton.contentHorizontalAlignment =UIControlContentHorizontalAlignmentLeft;
+//    
+//    [eButton setTitleEdgeInsets:UIEdgeInsetsMake(5,5, 0,0)];
+//    
+//    [eButton setImageEdgeInsets:UIEdgeInsetsMake(5,self.view.bounds.size.width - 25, 0,0)];
+//    
+//    //下显示线
+//    
+//    UILabel *label=[[UILabel alloc] initWithFrame:CGRectMake(0, hView.frame.size.height-1, hView.frame.size.width,1)];
+//    
+//    label.backgroundColor = [UIColor grayColor];
+//    [hView addSubview:label];
+//    
+//    [hView addSubview: eButton];
+//    
+//    return hView;
+//}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *allOrderCellIdentifier = @"allOrderCellIdentifier";
@@ -168,14 +170,14 @@
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
-    NSDictionary *dic = [_dataArray objectAtIndex:indexPath.section];
-    NSArray *array = [dic objectForKey:DIC_ARARRY];
-    
-    NSInteger row = indexPath.row;
+//    NSDictionary *dic = [_dataArray objectAtIndex:indexPath.section];
+//    NSArray *array = [dic objectForKey:DIC_ARARRY];
+//    
+//    NSInteger row = indexPath.row;
     
     [cell.textLabel setFont:[UIFont systemFontOfSize:13.0]];
     [cell addSubview:[self drawThreadWithFram:CGRectMake(15, 45.5, ScreenWidth-30, 0.5) andColor:[UIColor colorFromHexCode:@"e7e7e7"]]];
-    cell.textLabel.text = [array objectAtIndex:row];
+    cell.textLabel.text = @"分组名";
     [cell.textLabel setFont:[UIFont systemFontOfSize:14.0f]];
     cell.detailTextLabel.text = @"12";
     [cell.detailTextLabel setFont:[UIFont systemFontOfSize:12.0f]];
@@ -185,40 +187,40 @@
 
 #pragma mark --------------------------------------------------------------
 #pragma mark - Action
-- (void)expandButtonClicked:(UIButton *)sender {
-    UIButton *btn = (UIButton *)sender;
-    
-    NSInteger section = btn.tag;
-    
-    [self collapseOrExpand:section];
-    
-    //刷新数据
-    [self.groupTable reloadData];
-    
-}
-
-- (void)collapseOrExpand:(NSInteger)section {
-    NSMutableDictionary *dic = [_dataArray objectAtIndex:section];
-    
-    NSInteger expanded = [[dic objectForKey:DIC_EXPANDED] integerValue];
-    
-    if (expanded) {
-        [dic setValue:[NSNumber numberWithInt:0] forKey:DIC_EXPANDED];
-    }else {
-        [dic setValue:[NSNumber numberWithInt:1] forKey:DIC_EXPANDED];
-
-    }
-}
-
-
-- (NSInteger)isExpanded:(NSInteger)section {
-    
-    NSDictionary *dic=[_dataArray objectAtIndex:section];
-    
-    int expanded=[[dic objectForKey:DIC_EXPANDED] intValue];
-    
-    return expanded;
-
-}
+//- (void)expandButtonClicked:(UIButton *)sender {
+//    UIButton *btn = (UIButton *)sender;
+//    
+//    NSInteger section = btn.tag;
+//    
+//    [self collapseOrExpand:section];
+//    
+//    //刷新数据
+//    [self.groupTable reloadData];
+//    
+//}
+//
+//- (void)collapseOrExpand:(NSInteger)section {
+//    NSMutableDictionary *dic = [_dataArray objectAtIndex:section];
+//    
+//    NSInteger expanded = [[dic objectForKey:DIC_EXPANDED] integerValue];
+//    
+//    if (expanded) {
+//        [dic setValue:[NSNumber numberWithInt:0] forKey:DIC_EXPANDED];
+//    }else {
+//        [dic setValue:[NSNumber numberWithInt:1] forKey:DIC_EXPANDED];
+//
+//    }
+//}
+//
+//
+//- (NSInteger)isExpanded:(NSInteger)section {
+//    
+//    NSDictionary *dic=[_dataArray objectAtIndex:section];
+//    
+//    int expanded=[[dic objectForKey:DIC_EXPANDED] intValue];
+//    
+//    return expanded;
+//
+//}
 
 @end
